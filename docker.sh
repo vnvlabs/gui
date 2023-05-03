48be59fb54e76f5bd567ab906a9f6ada93f1573d
ben <boneill@rnet-tech.com>
2023-05-03 08:33:59 -0400

    fixing bug in theia. filesystem 1.37 introduces dependency on electron. Will switch to latest once this is removed

--- docker.sh
+++ docker.sh
@@ -1,6 +1,10 @@
 #Little script to build everything and launch a server
 #(the script is little -- this thing takes fooooorever to build)
 
-docker build -f docker/Dockerfile -t $1 .
+#Build a heavyweight version
+docker build -f docker/Dockerfile -t $1 --build-arg FROM_IMAGE=$2 .
+
+#Build the lightweight version
+docker build -f docker/Dockerfile -t $3 . 
 
 
